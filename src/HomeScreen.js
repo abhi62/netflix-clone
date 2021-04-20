3362f513da749e3cf4b5f46706188d942cf36f28
abhishek <abhi.leon.93@gmail.com>
2021-04-21 00:48:36 +0530

    scrolly added

---
+++ src/HomeScreen.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+      <Nav />
+
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+
+      {/* banner */}
+
+      {/* row */}
+    </div>
+  );
+}
+
+export default HomeScreen;
